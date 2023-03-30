00aebc06a6dd2fc7d898d5077bfb51233e824a8f
nehmundr <125494313+nehmundr@users.noreply.github.com>
2023-03-30 14:03:42 +0530

    Add files via upload

---
+++ client/components/SongDetail.js
@@ -0,0 +1,30 @@
+import React , {Component} from 'react';
+import {graphql} from 'react-apollo';
+import fecthSongDetails from '../queries/fecthSongDetails';
+import {Link} from 'react-router';
+import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
+
+class SongDetail extends Component {
+    
+    render(){
+        const {song}=this.props.data
+        if(!song){
+            return <div>Loading...</div>
+        }
+        return(
+            <div>
+                <Link to="/">Back</Link>
+                <h3>{song.title}</h3>
+                <LyricList lyrics={song.lyrics}/>
+                <LyricCreate songId={this.props.params.id}/>
+            </div>
+        )
+    }
+}
+
+export default graphql(fecthSongDetails,{
+    options:(props)=>{
+        return {variables:{id: props.params.id}}
+    }
+})(SongDetail);
